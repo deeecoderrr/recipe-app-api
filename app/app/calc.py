7c3212de60c012f5da83e23d20ca98af5e59ce7e
deeecoderrr <deecoderr@gmail.com>
2023-06-17 19:48:06 +0530

    calc

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+'''
+calculator functions
+'''
+
+def add(x, y):
+    '''Add x and y and returns x+y as result'''
+    return x+y
+
+def substract(x, y):
+    '''Substract x and y and returns x-y as result'''
+    return y-x
