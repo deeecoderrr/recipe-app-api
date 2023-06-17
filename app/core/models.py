d00ce45f185f42332d364177dda36788abf3a59c
deeecoderrr <deecoderr@gmail.com>
2023-06-18 00:35:42 +0530

    added super user functionality

--- app/core/models.py
+++ app/core/models.py
@@ -11,8 +11,18 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         ''' create save and return new user '''
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have a email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,3 +17,27 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_is_normalized(self):
+        ''' Test email is normalized for new user '''
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'Pass@123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_value_error(self):
+        ''' Test creating a user without email raises error '''
+        with self.assertRaises(ValueError):
+            user = get_user_model().objects.create_user('','Pass@123')
+
+    def test_create_super_user(self):
+        ''' Testing creating a superuser'''
+        user = get_user_model().objects.create_superuser('test1@example.com', 'Pass@123')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
